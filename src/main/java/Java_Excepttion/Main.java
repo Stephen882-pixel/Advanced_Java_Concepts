1f5b21a0e64ad38e5e063d85ac771d2c41109ea5
Stephen881 <stephenomondi005@outlook.com>
2024-10-18 19:31:01 +0300

    exception handling

---
+++ src/main/java/Java_Excepttion/Main.java
@@ -0,0 +1,55 @@
+/* The parent exceptions should always come last */
+
+package Java_Excepttion;
+
+class Area{
+    int l;
+    int w;
+    int area;
+
+    public void getArea(int l,int w){
+        area = l/w;
+       System.out.println(" The area is : " + area);
+   }
+
+
+}
+
+public class Main {
+    public static void main(String[] args){
+        int i = 2;
+        int j = 0;
+        int[] arr = new int[8];
+        String str = null;
+
+        try
+        {
+            j = 18/i;
+            System.out.println(str.length());
+            System.out.println(arr[8]);
+
+        }
+        catch (ArithmeticException e){
+            System.out.println("Something went wrong " + e);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Stay in your limit");
+        }
+        catch (Exception e){
+            System.out.println("Something went wrong " + e);
+        }
+        System.out.println(j);
+        System.out.println("Hello there");
+        Area obj = new Area();
+        try
+        {
+            obj.getArea(30,10);
+        }
+        catch (Exception e){
+            System.out.println("Something probably went wrong.Please check your dimensions " +  e);
+        }
+
+
+
+    }
+}
